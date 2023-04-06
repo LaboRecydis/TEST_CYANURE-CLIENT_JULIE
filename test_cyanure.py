5c3fbb4f6b3d9e0c69729a70cbff84b4507291d9
LaboRecydis <122175049+LaboRecydis@users.noreply.github.com>
2023-04-06 08:01:00 +0200

    Update test_cyanure.py

--- test_cyanure.py
+++ test_cyanure.py
@@ -20,7 +20,7 @@ if __name__=="__main__":
         layout="centered"
     )
 
-    st.title(" Fiche de non conformité ")
+    st.title(" Test cyanure - échantillon client_julie ")
 
     st.write("Auteur : Brahim AIT OUALI  - Technicien chimiste")
     st.write("### I. Identification")
